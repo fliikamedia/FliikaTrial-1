bc5e47ff00b67659842c10525eade604aaa65e20
fliikamedia <69017118+fliikamedia@users.noreply.github.com>
2020-08-20 21:06:05 -0400

    just build

---
+++ precache-manifest.e5dcec6a71f90d141a7fea6d9ead6c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea849c9afa19b1b967cf0cb0933f238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508fbbb6e2ad92e1255b",
+    "url": "/static/css/main.2c4b8ef4.chunk.css"
+  },
+  {
+    "revision": "53145863f315ed680ce6",
+    "url": "/static/js/2.af068be6.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.af068be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508fbbb6e2ad92e1255b",
+    "url": "/static/js/main.8f0b81bb.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
